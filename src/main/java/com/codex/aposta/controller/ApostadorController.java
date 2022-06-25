2f94168eb418d9d3fe7b470b8054ddbe27dfce76
Ivo Kriki <ivo.kriki@outlook.com>
2022-06-25 16:22:52 -0300

    refatorando classe ApostadorController

--- src/main/java/com/codex/aposta/controller/ApostadorController.java
+++ src/main/java/com/codex/aposta/controller/ApostadorController.java
@@ -3,6 +3,7 @@ package com.codex.aposta.controller;
 import com.codex.aposta.model.Apostador;
 import com.codex.aposta.model.dto.ApostadorIn;
 import com.codex.aposta.repository.ApostadorRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,13 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
+@RequiredArgsConstructor
 public class ApostadorController {
 
     private final ApostadorRepository apostadorRepository;
-
-    public ApostadorController(ApostadorRepository apostadorRepository) {
-        this.apostadorRepository = apostadorRepository;
-    }
 
     @PostMapping("/apostador")
     public ResponseEntity salvarApostador(@RequestBody ApostadorIn apostadorIn) {
