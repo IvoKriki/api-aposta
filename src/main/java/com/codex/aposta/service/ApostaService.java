e6798983a94843b63b1137feb369e52daf480a30
Ivo Kriki <ivo.kriki@outlook.com>
2022-06-27 14:17:23 -0300

    implementando try cath e log de erro com status code

--- src/main/java/com/codex/aposta/service/ApostaService.java
+++ src/main/java/com/codex/aposta/service/ApostaService.java
@@ -7,11 +7,15 @@ import com.codex.aposta.model.dto.ApostaOut;
 import com.codex.aposta.repository.ApostaRepository;
 import com.codex.aposta.repository.ApostadorRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.UUID;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -32,7 +36,7 @@ public class ApostaService {
             apostaRepository.save(aposta);
             apostaOut = new ApostaOut(numAposta, apostador.get().getNome(), apostador.get().getEmail());
         }catch (NoSuchElementException exception){
-            System.out.println("Deu ruim, usuario nao existe");
+            throw new ResponseStatusException(NOT_FOUND);
         }
 
         //return new ApostaOut(numAposta, apostador.get().getNome(), apostador.get().getEmail());
